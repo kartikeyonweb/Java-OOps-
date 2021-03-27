47a49086b96e3d232e28360442c7172b8bb43e01
Kartikey Narayan <55804161+kartikeyonweb@users.noreply.github.com>
2021-03-27 23:35:00 +0530

    Add files via upload

---
+++ XpY.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class XpY {
+
+public static void main(String args[]) {
+	
+	int a =3;
+	int b=3;
+	int result=1;
+	for(int i=0;i<b;i++) {
+		result=result*a;
+	}
+	
+		System.out.println(result);
+	}
+
+}
