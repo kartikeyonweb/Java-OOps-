7694e6b31b7d94739853da0aa2dbaaf7168f4de0
Kartikey Narayan <55804161+kartikeyonweb@users.noreply.github.com>
2021-03-28 23:08:47 +0530

    Add files via upload

---
+++ NestedFor.java
@@ -0,0 +1,15 @@
+ package loops;
+
+public class NestedFor {
+
+	public static void main(String[] args) {
+//		lets print 50 times 1-10
+for(int i=1;i<=50;i++) {
+	for(int j =1; j<=10;j++) {
+		System.out.println(j+ " ");
+	}
+System.out.println();
+}
+	}
+
+}
