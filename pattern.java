d07c34bac476417cc948914ef3ea574ca1c6cfcf
Kartikey Narayan <55804161+kartikeyonweb@users.noreply.github.com>
2021-03-31 23:34:41 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,14 @@
+package loops;
+
+public class pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int j=1;j<=5;j++) {
+	for(int i =1;i<=10;i++) {
+		System.out.printf("* ");
+	}
+	System.out.println();
+	}
+	}
+}
