832b6e7daa0a4352f41ba4876c5a5e1e537c4571
Kartikey Narayan <55804161+kartikeyonweb@users.noreply.github.com>
2021-04-07 23:42:49 +0530

    Add files via upload

---
+++ SeriesSum.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class SeriesSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n=90;
+float result =0;
+for (float i =1;i<=n;i++) {
+	result = result+1/i;
+}
+System.out.println(result);
+	}
+
+}
