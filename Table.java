7d130fc3408330d524a07548f188cc0d9b95ef6c
Kartikey Narayan <55804161+kartikeyonweb@users.noreply.github.com>
2021-04-08 23:43:11 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class Table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int j=1;j<=20;j++) {
+	int tableOF=j;
+
+	for(int i =1; i<=10;i=i+1){
+	System.out.printf(tableOF * i + "   ");
+	}
+System.out.println();	
+}
+	}
+}
